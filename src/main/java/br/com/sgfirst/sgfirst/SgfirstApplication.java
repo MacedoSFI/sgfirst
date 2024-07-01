85c276bd82032462a02d050c25cf11f88e21916f
felipe.nomade@yahoo.com <felipe.nomade@yahoo.com>
2024-07-01 19:10:12 -0300

    Projeto criado

---
+++ src/main/java/br/com/sgfirst/sgfirst/SgfirstApplication.java
@@ -0,0 +1,13 @@
+package br.com.sgfirst.sgfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgfirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sgfirst/sgfirst/SgfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sgfirst.sgfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgfirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
