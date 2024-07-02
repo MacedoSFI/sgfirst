0eafa4405cf66a7961c57e68a8228fd9ef6f4790
felipe.nomade@yahoo.com <felipe.nomade@yahoo.com>
2024-07-01 21:00:08 -0300

    criacao da estrutura de pacotes de funcionario e classe de domínio

---
+++ src/main/java/br/com/sgfirst/sgfirst/funcionario/api/FuncionarioRequest.java
@@ -0,0 +1,21 @@
+package br.com.sgfirst.sgfirst.funcionario.api;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
+
+@Value
+public class FuncionarioRequest {
+
+	@NotBlank
+	private String nomeCompleto;
+	
+	private String designacao;
+	
+	private Double salario;
+	
+	private String telefone;
+
+	private String endereco;
+	
+
+}
